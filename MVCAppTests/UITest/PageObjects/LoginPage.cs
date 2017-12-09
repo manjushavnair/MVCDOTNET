4de1af2aca5b7247c3cf3c2ad950627077241400
Manjusha Saju <manjushavnair@gmail.com>
2017-12-09 14:13:21 +0530

    ui testing added

---
+++ MVCAppTests/UITest/PageObjects/LoginPage.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+using MVCApp.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.PhantomJS;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Threading;
+using MVCAppTests.UITest.Common;
+using MVCAppTests.UITest.PageObjects;
+
+namespace MVCAppTests.UITest.PageObjects
+{
+ 
+    public class LoginPage
+    { 
+
+        public static string Name
+        {
+            get
+            {
+                var title = Driver.driver.FindElement(By.Id("loginLink"));
+                Console.WriteLine("title" + title);
+     
+                if (title != null)
+                    return title.Text;
+                return String.Empty;
+            }
+        }
+
+        public static bool register(string name)
+        {
+            
+
+            var bodyTag = Driver.driver.FindElement(By.Id("registerLink"));
+
+
+            Driver.driver.FindElement(By.Id("FirstName")).SendKeys("HoneyWell");
+            Thread.Sleep(2000);
+            Driver.driver.FindElement(By.Id("LastName")).SendKeys("Chennai");
+            Thread.Sleep(4000);
+            Driver.driver.FindElement(By.Id("LastName")).Clear();
+            Thread.Sleep(3000);
+            Driver.driver.FindElement(By.Id("LastName")).SendKeys("Bangalore");
+
+            Thread.Sleep(3000);
+
+            Driver.driver.FindElement(By.Id("FirstName")).SendKeys(Keys.Enter); 
+            //Driver.driver.FindElement(By.XPath("//input[@value='Create']")).Click();
+
+            return bodyTag.Text.Contains(name);
+        }
+ 
+    }
+}
---
+++ MVCAppTests/UITest/TestCases/WUITest.cs
@@ -0,0 +1,89 @@
+﻿using NUnit.Framework; 
+using MVCApp.Controllers;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.PhantomJS;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Threading;
+using MVCAppTests.UITest.Common;
+using MVCAppTests.UITest.PageObjects;
+
+namespace MVCAppTests.UITest.TestCases
+{
+
+    
+    [TestClass]
+    public class WUITest :UITest
+    {
+       
+        [TestMethod]
+        [TestCategory("Selenium")]
+        [Priority(1)]
+        public void Can_Navigate_To_GoToHome()
+
+        {
+
+            HomePage.GoTo();
+
+            System.Diagnostics.Debug.WriteLine(HomePage.Name);
+            Console.WriteLine(HomePage.Name);
+            Console.Write("MyPage.Name" + HomePage.Name);
+            Driver.driver.Manage().Window.Maximize();
+
+            HomePage.home("Log in");
+            Driver.driver.FindElement(By.Id("loginLink")).Click();
+
+            Thread.Sleep(2000); 
+            NUnit.Framework.Assert.AreEqual(HomePage.Name, "Log in");
+          
+             
+        }
+       
+
+        [TestMethod]
+        [TestCategory("Selenium")]
+        [Priority(2)]
+        public void Can_Navigate_To_Login()
+
+
+        {
+
+            HomePage.home("Log in");
+            Driver.driver.Manage().Window.Maximize();
+
+            Thread.Sleep(5000);
+
+            Driver.driver.FindElement(By.Id("loginLink")).Click();
+
+
+            Thread.Sleep(8000);
+
+            Driver.driver.FindElement(By.Id("registerLink")).Click();
+
+            LoginPage.register("Join Us"); 
+
+            Thread.Sleep(8000);
+
+            NUnit.Framework.Assert.AreEqual(LoginPage.Name, "Join Us");
+
+ 
+
+
+
+
+        }
+
+   
+
+    }
+}
